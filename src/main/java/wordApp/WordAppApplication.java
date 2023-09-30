9767dd0d0b7bb3813fd0840ea503fb811c9f1d5f
Yijia Chen <kevinc110604@gmail.com>
2023-09-30 01:24:41 -0700

    [new] Init commit

---
+++ src/main/java/wordApp/WordAppApplication.java
@@ -0,0 +1,13 @@
+package wordApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordAppApplication.class, args);
+	}
+
+}
